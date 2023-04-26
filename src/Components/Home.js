6f9e12acd4ddb47dc1f362f2be560f7c562b029e
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-04-26 17:30:34 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeComponent() {
   return (
-    <div className='heading'>
+    <div className='home_Container'>
       <h1>Customize Bootstrap Navbar</h1>
       <p>Easy way to change navbar background, text and hover link color in bootstrap.</p>
     </div>
